a48eace126b9de00d4920e4098c4588027cc2843
Martin Jansa <martin.jansa@lge.com>
2018-06-16 11:42:50 +0000

    build.sh: fix checking out meta-ros and meta-r2d2 (v6.10.2e)
    * without this verification and GPVB jenkins builds were always building
      just master (or whatever is specified in weboslayers.py)
    
    Change-Id: Idc9c681ebb38606fb28c84f0e8a5d479cc7e46f8

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,12 +18,14 @@
 #set -x
 
 # Some constants
-SCRIPT_VERSION="6.10.2d"
+SCRIPT_VERSION="6.10.2e"
 SCRIPT_NAME=`basename $0`
 AUTHORITATIVE_OFFICIAL_BUILD_SITE="svl"
 
 BUILD_REPO="r2d2/build-r2d2"
-BUILD_LAYERS=("webosose/meta-webosose r2d2/meta-ros r2d2/meta-r2d2 ")
+BUILD_LAYERS=("webosose/meta-webosose"
+              "r2d2/meta-ros"
+              "r2d2/meta-r2d2")
 
 # Create BOM files, by default disabled
 CREATE_BOM=
